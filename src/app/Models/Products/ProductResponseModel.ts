eb4881cb468be62aee0730212b53263b966c30ee
Berkant Karaca <93256643+berkantkaraca@users.noreply.github.com>
2025-12-24 16:06:49 +0300

    product modeli oluşturuldu

---
+++ src/app/Models/Products/ProductResponseModel.ts
@@ -0,0 +1,10 @@
+import { BaseProductViewModel } from "./BaseProductViewModel";
+
+export class ProductResponseModel extends BaseProductViewModel {
+    id: number;
+
+    constructor(id: number, productName: string, unitPrice: number, categoryId: number) {
+        super(productName, unitPrice, categoryId);
+        this.id = id;
+    }
+}
